000d0a0447aaa61d0ff6febe70af4b5a2a1f3762
Allan-Sanchez <asanchezrixtun@gmail.com>
2021-12-03 11:56:40 -0600

    feat: read data to firebase firestore

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useContext, useState, useEffect } from "react";
+import CardDish from "../../components/ui/CardDish";
+import { FirebaseContext } from "../../firebase";
 export default function index() {
-    return (
-        <div>
-            home
-        </div>
-    )
+  const { firebase } = useContext(FirebaseContext);
+  const [data, setData] = useState([]);
+
+  const response = async () => {
+    const res = await firebase.getCollections("dishes");
+    setData(res);
+  };
+
+  useEffect(() => {
+    response();
+  }, []);
+
+  return (
+    <>
+      <div className="grid xl:grid-cols-2">
+        {data.map((item) => (
+          <CardDish key={item.id} data={item} />
+        ))}
+      </div>
+    </>
+  );
 }
